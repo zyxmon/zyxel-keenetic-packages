1197bb52bb819c568007c883ad445030d4891d62
zyxmon@gmail.com <zyxmon@gmail.com@65668616-8d02-ea0d-c813-071bedccfd1c>
2011-12-16 09:22:47 +0000

    moving to new zyxel kernel sources - see History file

--- trunk/package/kernel/modules/wdt.mk
+++ trunk/package/kernel/modules/wdt.mk
@@ -15,3 +15,17 @@ define KernelPackage/rt_timer/description
 endef
 
 $(eval $(call KernelPackage,rt_timer))
+
+define KernelPackage/ralink_wdt
+  SUBMENU:=$(WDT_MENU)
+  TITLE:=New Ralink Watchdog and Soft Timer
+  KCONFIG:=CONFIG_RALINK_TIMER
+  FILES:=$(LINUX_DIR)/arch/mips/rt2880/ralink_wdt.$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,10,rt_timer)
+endef
+
+define KernelPackage/ralink_wdt/description
+  New Ralink Timer Module.
+endef
+
+$(eval $(call KernelPackage,ralink_wdt))
